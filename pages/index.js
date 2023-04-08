22daa8d770367fa4bd2329db4e5014525d2d976a
ali <moini_alessandro_13@yahoo.com>
2023-04-08 13:12:49 +0430

    adding meta and title

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import Head from 'next/head';
 export default function Home() {
   return (
     <div className="bg-red-500">
+      <Head>
+        <title>Buy Pure Saffron Online - Best Quality Saffron Shop</title>
+        <meta
+          name="description"
+          content="Looking to buy pure saffron online? Look no further than our saffron shop. We offer the best quality saffron sourced directly from trusted farmers. Our saffron is 100% pure and organic, with no additives or preservatives. Shop with us today for the finest saffron at competitive prices."
+        />
+      </Head>
       <h1 className="text-3xl font-bold">hello world</h1>
     </div>
   );
