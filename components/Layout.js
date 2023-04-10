e82da75a51ad5469afec353541bb77b3b41c10e4
ali <moini_alessandro_13@yahoo.com>
2023-04-10 11:21:14 +0430

    productscreen

--- components/Layout.js
+++ components/Layout.js
@@ -18,20 +18,24 @@ export default function Layout({ title, children }) {
       </Head>
       <div className="flex min-h-screen flex-col justify-between">
         <header className="">
-          <nav className="flex h-12 justify-between items-center shadow-md">
+          <nav className="flex px-4 h-12 justify-between items-center shadow-md">
+            <div className="flex justify-center items-center">
+              <Link href="/cart" className="p-2   border-r-2">
+                <BsCart3 />
+              </Link>
+              <Link href="/login" className="p-2">
+                ورود
+              </Link>
+            </div>
             <Link href="/" className="text-lg font-bold saffron-color">
               زعفران نطنز
             </Link>
-            <div>
-              <Link href="/cart">
-                <BsCart3 />
-              </Link>
-              <Link href="/login">Login</Link>
-            </div>
           </nav>
         </header>
-        <main>{children}</main>
-        <footer>footer</footer>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          footer
+        </footer>
       </div>
     </>
   );
