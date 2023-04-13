483d51034f960b0b6f5c320bde1fe0e7e9c47462
ali <moini_alessandro_13@yahoo.com>
2023-04-13 11:08:38 +0430

    connect db and seeding

---
+++ pages/api/seed.js
@@ -0,0 +1,16 @@
+import db from '../../utils/db';
+import data from './../../utils/data';
+
+import User from './../../models/User';
+
+const handler = async (req, res) => {
+  await db.connect();
+  await User.deleteMany();
+  await User.insertMany(data.users);
+  //   await Product.deleteMany();
+  //   await Product.insertMany(data.products);
+  await db.disconnect();
+  res.send({ message: 'seeded successfuly' });
+};
+
+export default handler;
