d8a02c3e7243ecc5ad58ea0f68c94b0340163043
ali <moini_alessandro_13@yahoo.com>
2023-05-03 12:04:28 +0430

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,23 +2,31 @@ import '@/styles/globals.css';
 import { SessionProvider, useSession } from 'next-auth/react';
 import { StoreProvider } from '@/utils/Store';
 import { useRouter } from 'next/router';
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <StoreProvider>
-        {Component.auth ? (
-          <Auth>
+    <>
+      <SessionProvider session={session}>
+        <StoreProvider>
+          {Component.auth ? (
+            <Auth>
+              <Component {...pageProps} />
+            </Auth>
+          ) : (
             <Component {...pageProps} />
-          </Auth>
-        ) : (
-          <Component {...pageProps} />
-        )}
-      </StoreProvider>
-    </SessionProvider>
+          )}
+        </StoreProvider>
+      </SessionProvider>
+      <Analytics
+        beforeSend={(e) => {
+          if (e.url.includes('private')) return null;
+          return e;
+        }}
+      />
+    </>
   );
 }
 
